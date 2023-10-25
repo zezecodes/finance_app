0a5f0cd2594201a0916b43d6d6dd1ad7d7237f00
Aaron Djangmah <96618832+zezecodes@users.noreply.github.com>
2023-10-25 13:00:03 +0000

    commits

--- lib/presentation/homepage/presentation/pages/homepage.dart
+++ lib/presentation/homepage/presentation/pages/homepage.dart
@@ -15,190 +15,204 @@ class _HomepageState extends State<Homepage> {
     final width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
-        child: Stack(
-          clipBehavior: Clip.none,
+        child: Column(
           children: [
-            Container(
-              width: width,
-              height: height * 0.3,
-              decoration: const BoxDecoration(
-                  color: Color.fromARGB(255, 113, 199, 185),
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(30),
-                      bottomRight: Radius.circular(30))),
-              child: Column(
-                children: [
-                SizedBox(height: height * 0.02,),
-                SizedBox(
-                  width: width * 0.9,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
+            Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Container(
+                  width: width,
+                  height: height * 0.25,
+                  decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 113, 199, 185),
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(30),
+                          bottomRight: Radius.circular(30))),
+                  child: Column(children: [
+                    SizedBox(
+                      height: height * 0.02,
+                    ),
+                    SizedBox(
+                      width: width * 0.9,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text(
-                            "Good Afternoon",
-                            style: GoogleFonts.inter(
-                                fontSize: 18, color: Colors.white),
-                          ),
-                          Text(
-                            "Aaron Djangmah",
-                            style: GoogleFonts.inter(
-                                fontSize: 22,
-                                color: Colors.white,
-                                fontWeight: FontWeight.w600),
-                          )
-                        ],
-                      ),
-                      Container(
-                          width: width * 0.1,
-                          height: height * 0.05,
-                          decoration: BoxDecoration(
-                              color: Colors.black,
-                              borderRadius: BorderRadius.circular(10)),
-                          child: IconButton(
-                              onPressed: () {},
-                              icon: const Icon(
-                                Icons.notifications,
-                                color: Colors.white,
-                              )))
-                    ],
-                  ),
-                ),
-              ]),
-            ),
-            Positioned(
-                top: height * 0.2,
-                left: width * 0.1,
-                child: Column(
-                  children: [
-                    Container(
-                      width: width * 0.8,
-                      height: height * 0.2,
-                      decoration: BoxDecoration(
-                          color: const Color.fromARGB(255, 61, 129, 118),
-                          borderRadius: BorderRadius.circular(30)),
-                      child: Column(
-                        // crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          SizedBox(
-                            width: width * 0.7,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  "Total Balance",
-                                  style: GoogleFonts.inter(
-                                      fontSize: 20, color: Colors.white),
-                                ),
-                                IconButton(
-                                    onPressed: () {},
-                                    icon: const Icon(
-                                      Icons.more_horiz,
-                                      color: Colors.white,
-                                    ))
-                              ],
-                            ),
-                          ),
-                          Text(
-                            "2,400.00",
-                            style: GoogleFonts.inter(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w500,
-                                color: Colors.white),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
+                          Column(
                             children: [
-                              Column(
-                                children: [
-                                  Row(
-                                    children: [
-                                      CircleAvatar(
-                                        radius: 15,
-                                        backgroundColor: const Color.fromARGB(
-                                            255, 113, 199, 185),
-                                        child: IconButton(
-                                            onPressed: () {},
-                                            icon: const Icon(
-                                              Icons.arrow_downward,
-                                              size: 15,
-                                              color: Colors.white,
-                                            )),
-                                      ),
-                                      SizedBox(width: width * 0.02,),
-                                      Text(
-                                        "Income",
-                                        style: GoogleFonts.inter(
-                                            fontSize: 15, color: Colors.white),
-                                      ),
-                                    ],
-                                  ),
-                                  Text("1,400",
-                                      style: GoogleFonts.inter(
-                                          fontSize: 13, color: Colors.white))
-                                ],
+                              Text(
+                                "Good Afternoon",
+                                style: GoogleFonts.inter(
+                                    fontSize: 18, color: Colors.white),
                               ),
-                              SizedBox(
-                                width: width * 0.25,
-                              ),
-                              Column(
-                                children: [
-                                  Row(
-                                    children: [
-                                      CircleAvatar(
-                                        radius: 15,
-                                        backgroundColor: const Color.fromARGB(
-                                            255, 113, 199, 185),
-                                        child: IconButton(
-                                            onPressed: () {},
-                                            icon: const Icon(
-                                              Icons.arrow_upward,
-                                              size: 15,
-                                              color: Colors.white,
-                                            )),
-                                      ),
-                                      SizedBox(width: width * 0.02,),
-                                      Text(
-                                        "Expenses",
-                                        style: GoogleFonts.inter(
-                                            fontSize: 15, color: Colors.white),
-                                      ),
-                                    ],
-                                  ),
-                                  Text(
-                                    "249",
-                                    style: GoogleFonts.inter(
-                                        fontSize: 13, color: Colors.white),
-                                  )
-                                ],
-                              ),
+                              Text(
+                                "Aaron Djangmah",
+                                style: GoogleFonts.inter(
+                                    fontSize: 22,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w600),
+                              )
                             ],
-                          )
+                          ),
+                          Container(
+                              width: width * 0.1,
+                              height: height * 0.05,
+                              decoration: BoxDecoration(
+                                  color: Colors.black,
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    Icons.notifications,
+                                    color: Colors.white,
+                                  )))
                         ],
                       ),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  ]),
+                ),
+                Positioned(
+                    top: height * 0.15,
+                    left: width * 0.1,
+                    child: Column(
                       children: [
-                        Text(
-                          "Transaction History",
-                          style: GoogleFonts.inter(
-                              fontSize: 20, fontWeight: FontWeight.w600),
+                        Container(
+                          width: width * 0.8,
+                          height: height * 0.2,
+                          decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 61, 129, 118),
+                              borderRadius: BorderRadius.circular(20)),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              SizedBox(
+                                width: width * 0.7,
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "Total Balance",
+                                      style: GoogleFonts.inter(
+                                          fontSize: 20, color: Colors.white),
+                                    ),
+                                    IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(
+                                          Icons.more_horiz,
+                                          color: Colors.white,
+                                        ))
+                                  ],
+                                ),
+                              ),
+                              Text(
+                                "2,400.00",
+                                style: GoogleFonts.inter(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Column(
+                                    children: [
+                                      Row(
+                                        children: [
+                                          CircleAvatar(
+                                            radius: 15,
+                                            backgroundColor:
+                                                const Color.fromARGB(
+                                                    255, 113, 199, 185),
+                                            child: IconButton(
+                                                onPressed: () {},
+                                                icon: const Icon(
+                                                  Icons.arrow_downward,
+                                                  size: 15,
+                                                  color: Colors.white,
+                                                )),
+                                          ),
+                                          SizedBox(
+                                            width: width * 0.02,
+                                          ),
+                                          Text(
+                                            "Income",
+                                            style: GoogleFonts.inter(
+                                                fontSize: 15,
+                                                color: Colors.white),
+                                          ),
+                                        ],
+                                      ),
+                                      Text("1,400",
+                                          style: GoogleFonts.inter(
+                                              fontSize: 13,
+                                              color: Colors.white))
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    width: width * 0.25,
+                                  ),
+                                  Column(
+                                    children: [
+                                      Row(
+                                        children: [
+                                          CircleAvatar(
+                                            radius: 15,
+                                            backgroundColor:
+                                                const Color.fromARGB(
+                                                    255, 113, 199, 185),
+                                            child: IconButton(
+                                                onPressed: () {},
+                                                icon: const Icon(
+                                                  Icons.arrow_upward,
+                                                  size: 15,
+                                                  color: Colors.white,
+                                                )),
+                                          ),
+                                          SizedBox(
+                                            width: width * 0.02,
+                                          ),
+                                          Text(
+                                            "Expenses",
+                                            style: GoogleFonts.inter(
+                                                fontSize: 15,
+                                                color: Colors.white),
+                                          ),
+                                        ],
+                                      ),
+                                      Text(
+                                        "249",
+                                        style: GoogleFonts.inter(
+                                            fontSize: 13, color: Colors.white),
+                                      )
+                                    ],
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
                         ),
-                        SizedBox(width: width * 0.1),
-                        TextButton(
-                            onPressed: () {},
-                            child: Text(
-                              "See all",
-                              style: GoogleFonts.inter(
-                                  color: Colors.grey, fontSize: 15),
-                            ))
                       ],
-                    ),
-                  ],
-                )),
+                    )),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Transaction History",
+                  style: GoogleFonts.inter(
+                      fontSize: 20, fontWeight: FontWeight.w600),
+                ),
+                SizedBox(width: width * 0.1),
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "See all",
+                      style:
+                          GoogleFonts.inter(color: Colors.grey, fontSize: 15),
+                    ))
+              ],
+            ),
           ],
         ),
       ),
